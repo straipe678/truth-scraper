597e91b4b528ddf4dcfdc20d9bf48736d00d2094
straipe678 <straipe@fastmail.com>
2025-04-13 02:02:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/api/trump-posts", async (req, res) => {
     const browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(),
+      executablePath: await chromium.executablePath,
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     });
