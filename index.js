6ca2e987580274c7e5af62d957e8ca64d5316f04
straipe678 <straipe@fastmail.com>
2025-04-12 17:37:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const rateLimit = require("express-rate-limit");
 const cors = require("cors");
 
